c948c8ec66c2566ab147abd93d314bc614c660f2
alex <alex.streams.data@gmail.com>
2021-02-02 01:00:18 -0500

    add CookiecutterDS package + dockerfile

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A template repo for datascience using docker',
+    author='(AUTHOR NAME HERE)',  # Change to your name here
+    license='',
+)
